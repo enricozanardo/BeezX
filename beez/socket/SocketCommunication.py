3e1e72b87ab172f21069430285d190b1f1ec61be
OneZero Binary LTD <ezanardo@onezerobinary.com>
2022-05-31 16:46:53 +0200

    check Block

--- beez/socket/SocketCommunication.py
+++ beez/socket/SocketCommunication.py
@@ -108,7 +108,7 @@ class SocketCommunication(Node):
         elif message.messageType == MessageType.BLOCK.name:
             # handle the BLOCK
             logger.info(f"A BLOCK Message will be broadcasted!! {message.messageType}")
-            blockTX : BlockTX = message.block
+            block : Block = message.block
 
-            logger.info(f"{type(blockTX)}")
-            self.beezNode.handleBlock(blockTX)
+            logger.info(f"{type(block)}")
+            self.beezNode.handleBlock(block)
